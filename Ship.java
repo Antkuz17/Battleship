246dde956db8f8eda3fceddb7034ffb6f0ef7926
Anton <Antonkuzmichev17@gmail.com>
2025-06-23 10:11:46 -0400

    Fixed validation of initial position of ship

--- Ship.java
+++ Ship.java
@@ -22,23 +22,56 @@ public class Ship {
     public void placeShip(){
         System.out.println("Where do you want the " + (length -1) + " ship to start?: ");
         String potentialPos = input.nextLine(); // Potential pos still needs to be checked for validity
-        Coord initPos = translation(potentialPos);
+        Coord initPos = translation(potentialPos); // Translates from String "A1" to coordinate notation 0,0
         while (!isOnGrid(initPos)) {
              System.out.println("Not on the grid try again");
              System.out.println("Where do you want the " + (length - 1) + " ship to start?: ");
              potentialPos = input.nextLine();
              initPos = translation(potentialPos);
         }
+        boolean answer = initPosValid(initPos);
+        System.out.println(answer);
+
+
+
     }
 
     /**
-     * Returns whether or no the initial position is a valid on
+     * Returns whether or not a position is a valid one
+     * 
+     * Valid - At least one orientation in that initial pose will result in a legal position
+     * 
+     * Logic- Checks above and below the initpose to make sure that is a point where the ship would fit
      * 
      * @return boolean value of true if its placeable and false if not
      */
-    //public boolean initPosValid(){ 
+    public boolean initPosValid(Coord pose){ 
+        boolean rightValid = true;
+        boolean leftValid = true;
+        boolean upValid = true;
+        boolean downValid = true;
 
-    //}
+        if((pose.getRow() + (length-1)) > 9){
+            downValid = false;
+        }
+        if ((pose.getRow() - (length - 1)) < 0) {
+            upValid = false;
+        }
+        if ((pose.getCol() - (length - 1)) < 0) {
+            leftValid = false;
+        }
+        if ((pose.getCol() + (length - 1)) > 9) {
+            rightValid = false;
+        }
+        if(downValid == false && upValid == false && leftValid == false && rightValid == false){
+            return false;
+        }
+        System.out.println("Right Valid: " + rightValid);
+        System.out.println("Left Valid: " + leftValid);
+        System.out.println("Up Valid: " + upValid);
+        System.out.println("Down Valid" + downValid);
+        return true;
+    }
     /**
      * Returns a true or false depending on whether the given coordinate is on the grd
      * @return Return true if on grid and false if the coord is off grid
@@ -65,7 +98,7 @@ public class Ship {
             secondNumber = 11;
         } 
         else if (Integer.parseInt(pos.substring(1)) == 10){
-            secondNumber =10;
+            secondNumber =9;
         }
         else {
             secondNumber = Character.getNumericValue(pos.charAt(1)) - 1;
